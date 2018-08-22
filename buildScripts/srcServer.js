e170e666d437270fc250fd7ca6ee32f1dfd73087
Kieran Mahony <kieranmahony@kierans-air-2.home>
2018-08-22 16:50:25 +0100

    Http Calls / Api / Mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,13 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res) {
+  res.json([
+    {"id": 1, "firstName": "Kieran", "lastName": "Mahony", "email": "x@y"},
+    {"id": 2, "firstName": "Kieran2", "lastName": "Mahony2", "email": "x@y"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
