84c964608058721ee7a8efaef63cdb6412ce2a1f
Kieran Mahony <kieranmahony@kierans-air-2.home>
2018-09-15 14:18:06 +0100

    Production Building

---
+++ buildScripts/distServer.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req,res) {
+  res.json([
+    {"id": 1, "firstName": "Kieran", "lastName": "Mahony", "email": "x@y"},
+    {"id": 2, "firstName": "Kieran2", "lastName": "Mahony2", "email": "x@y"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else
+  {
+    open('http://localhost:' + port);
+  }
+});
+
